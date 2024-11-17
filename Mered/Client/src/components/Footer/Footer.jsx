704309f3279e8dffeba881f40a45dc51944432e8
540mhz <georgijhamov5@gmail.com>
2024-11-17 20:52:51 +0200

    added footer

--- Mered/Client/src/components/Footer/Footer.jsx
+++ Mered/Client/src/components/Footer/Footer.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+
+import './Footer.css';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+    <footer className="footer">
+      
+      <div className="footer-column">
+        <h3>CUSTOMER SUPPORT</h3>
+        <ul>
+          <li><a href="#track">Track your order</a></li>
+          <li><a href="#return">Return your order</a></li>
+          <li><a href="#terms">Terms</a></li>
+          <li><a href="#privacy">Privacy Policy</a></li>
+          <li><a href="#contact">Contact us</a></li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h3>ABOUT MERED WEAR</h3>
+        <ul>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#podcast">Podcast</a></li>
+          <li><a href="#story">Our Story</a></li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h3>BLA BLA BLA</h3>
+        <ul>
+          <li><a href="#blabla">Bla Blabla</a></li>
+          <li><a href="#blabla2">Blabla</a></li>
+          <li><a href="#blabla3">Blablabla Bla</a></li>
+        </ul>
+      </div>
+      
+    </footer>
+    <div className="footer-copyright">
+        Copyright ©2024, MERED Wear Israel
+      </div>
 
-export default Footer
+    </>
+    
+  );
+};
+
+export default Footer;
