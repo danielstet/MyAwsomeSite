212382ef920d89b98618d43a2bd11553bd5b39ed
danielstet <stetsyukdaniel@gmail.com>
2024-11-26 20:03:19 +0200

    backup

--- Mered/Client/src/components/FAQDropBox/FAQDropBox.jsx
+++ Mered/Client/src/components/FAQDropBox/FAQDropBox.jsx
@@ -8,29 +8,53 @@ import './FAQDropBox.css'
 const FAQDropBox = ({Number ,Question, Answer}) => {
     const [showAnswer, setShowAnswer] = useState(false);
 
-    return (
-        <div id='Question-Wrapper'>
-            {Number < 10 ? 
-                <p id='Number'>0{Number}</p> : 
-                <p id='Number'>{Number}</p>
-            }
-            <div>
-                <p id='Question'>
-                    {Question}
+    // return (
+    //     <div id='Question-Wrapper'>
+    //         {Number < 10 ? 
+    //             <p id='Number'>0{Number}</p> : 
+    //             <p id='Number'>{Number}</p>
+    //         }
+    //         <div id='Content'>
+    //             <p id='Question'>
+    //                 {Question}
+    //             </p>
+    //             { showAnswer && (
+    //                 <p id='Answer'>
+    //                     {Answer}
+    //                 </p>
+    //             )
+    //         }
+    //         </div>
+    //         <div id='showAnswer' onClick={() => setShowAnswer(!showAnswer)}> {!showAnswer ? 
+    //             <img src={plus} width={20} height={20}></img> : 
+    //             <img src={minus} width={20} height={20}></img> }
+    //         </div>
+            
+    //     </div>
+    // )
+
+     return (
+        <div id="Question-Wrapper">
+            {Number < 10 ? (
+                <p id="Number">0{Number}</p>
+            ) : (
+                <p id="Number">{Number}</p>
+            )}
+            <div id="Content">
+                <p id="Question">{Question}</p>
+                <p id={`Answer ${showAnswer ? 'show' : ''}`}>
+                    {Answer}
                 </p>
-                { showAnswer && (
-                    <p id='Answer'>
-                        {Answer}
-                    </p>
-                )
-            }
             </div>
-            <div id='showAnswer' onClick={() => setShowAnswer(!showAnswer)}> {!showAnswer ? 
-                <img src={plus} width={20} height={20}></img> : 
-                <img src={minus} width={20} height={20}></img> }
+            <div id="showAnswer" onClick={() => setShowAnswer(!showAnswer)}>
+                {!showAnswer ? (
+                    <img src={plus} width={20} height={20} alt="Expand" />
+                ) : (
+                    <img src={minus} width={20} height={20} alt="Collapse" />
+                )}
             </div>
-        </div>
-    )
+        </div> 
+        )
 }
 
 export default FAQDropBox
--- Mered/Client/src/pages/FAQPage/FAQ.jsx
+++ Mered/Client/src/pages/FAQPage/FAQ.jsx
@@ -3,9 +3,14 @@ import "./FAQ.css"
 import DropBox from '../../components/FAQDropBox/FAQDropBox'
 
 const FAQ = () => {
+  let id = 1;
+
   return (
     <div id="wrapper">
-      <DropBox Number={1} Question="Test" Answer="Lorem ipsum" />
+      <DropBox Number={id++} Question="Who is gay?" Answer="Not me!" />
+      <DropBox Number={id++} Question="Do you smell your fingers after scratching your balls?" Answer="Well you know I do ;)" />
+      <DropBox Number={id++} Question="What is the best coding language?" Answer="JS is the best, f#ck python!" />
+      <DropBox Number={id++} Question="Who has bigger Daniel Or Georgey?" Answer="Georgy has the bigger heart <3" />
     </div>
   )
 }
